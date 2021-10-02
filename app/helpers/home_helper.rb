caad83ceec34bf585c1b7f2789aff6b5e75b22cb
jongwoniee <ljw3728@gmail.com>
2021-10-02 02:30:23 -0400

    mobile test

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
 end
